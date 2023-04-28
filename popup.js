d7543d07af0cce9086d716881268d5b65ab2ebe2
chanychi <chan.ychi@gmail.com>
2023-04-27 23:33:10 -0500

    Added localStorage to store list of endorsed user

--- popup.js
+++ popup.js
@@ -8,6 +8,21 @@ btn.addEventListener("click", async () => {
     chrome.scripting.executeScript({ // Run the following script on our tab
       target: {tabId: tab.id},
       function: () => {
+        const currURLWindow = window.location.href;
+        const url = new URL('', currURLWindow);
+        const containsProfile = url.pathname.includes("/in/");
+        const getConnectionName = document.querySelector("h1")
+        let listOfEndorsed = [];
+
+        if(!getConnectionName?.innerHTML) return;
+        if(url.hostname !== "www.linkedin.com" && !containsProfile) return;
+
+        if(containsProfile) {
+          let storedNames = JSON.parse(localStorage.getItem('ListOfEndorsed') || "[]");
+          listOfEndorsed = [...storedNames, getConnectionName?.innerHTML];
+          localStorage.setItem('ListOfEndorsed', JSON.stringify(listOfEndorsed));
+        }
+
         document.querySelectorAll('.pvs-navigation__text').forEach((i) => {
           if(i.innerHTML.indexOf('skills') > -1) {
             i.parentElement.click()
@@ -15,7 +30,7 @@ btn.addEventListener("click", async () => {
             setTimeout(function() {
               window.scrollTo(0, document.body.scrollHeight - 500);
               const skills = document.querySelectorAll('.pv2 > .artdeco-button--muted');
-              
+
               for (let i = 0; i < skills.length; ++i) {
                 const btn = skills[i];
                 const endorsed = !!btn.querySelector('.artdeco-button__icon')
@@ -26,7 +41,6 @@ btn.addEventListener("click", async () => {
               }, 2000)
             }
           });
-
         }
     })
 })
