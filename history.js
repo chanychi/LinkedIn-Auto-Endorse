e1c4d26151469c2433b67a2c93142e97548f4502
chanychi <chan.ychi@gmail.com>
2023-05-04 23:09:53 -0500

    Refactored history

--- history.js
+++ history.js
@@ -1,10 +1,40 @@
+const STORAGE = {
+  'key': 'ListOfEndorsed'
+}
+
+const HISTORY_MAX = {
+  'size': 5
+}
+
+const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key) || '[]');
+
+const setLocalStorage = (collection) => {
+  if (collection?.length) localStorage.setItem(STORAGE.key, JSON.stringify(collection));
+  return getLocalStorage(STORAGE.key);
+}
+
+const getLastFive = (collection) => collection > `${HISTORY_MAX.size}` ? collection.slice(-HISTORY_MAX.size) : collection.length ? collection : [];
+
+const renderFive = (collection) => {
+  const span = document.querySelector('span');
+  span.innerHTML = '';
+
+  const nodes = collection.map(item => {
+    const textNode = document.createTextNode(item);
+    const separatorNode = document.createTextNode(' || ');
+    return [textNode, separatorNode];
+  }).flat().slice(0, -1);
+
+  nodes.forEach(node => span.append(node));
+}
+
 async function createHistory() {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
   const result = await chrome.scripting.executeScript({
+    args: [STORAGE],
     target: { tabId: tab.id },
-    function: () => {
-      return [...JSON.parse(localStorage.getItem('ListOfEndorsed') || "[]")];
+    function: (storage) => {
+      return [...JSON.parse(localStorage.getItem(storage.key) || "[]")];
     }
   })
   return result[0].result;
@@ -13,27 +43,7 @@ async function createHistory() {
 createHistory query the tab's localStorage and returns a promise. The promise is passed to the popup to store it in the chrome's storage - allowing it to persist when the user is not on LinkedIn
 */
 createHistory()
-  .then((data) => {
-    const span = document.querySelector('span')
-    const extensionStorage = JSON.parse(localStorage.getItem('ListOfEndorsed') || "[]");
-    let lastFive;
-
-    if (data?.length) localStorage.setItem('ListOfEndorsed', JSON.stringify(data));
-
-    if (extensionStorage.length > 5) {
-      lastFive = extensionStorage.slice(-5);
-    } else if (extensionStorage.length !== 0) {
-      lastFive = extensionStorage;
-    }
-
-    for (let i = 0; i < lastFive?.length; i++) {
-      if (lastFive[i + 1]) {
-        const textNode = document.createTextNode(lastFive[i] + ' || ');
-        span.append(textNode);
-        continue;
-      }
-      const textNode = document.createTextNode(lastFive[i]);
-      span.append(textNode);
-    }
-  })
+  .then(setLocalStorage)
+  .then(getLastFive)
+  .then(renderFive)
   .catch((error) => console.log(error));
